0ba80a9eef758e3efd7ac97e91dcdf404cee2602
prashant kumar <prashantmanwan@gmail.com>
2023-05-18 12:13:02 +0530

    Home content improved

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Live Url
 > https://listedassignment.onrender.com/
+
 > https://prashant-listedassignment.netlify.app/
+
 > https://listed-assignment-navy.vercel.app/
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -61,24 +61,23 @@ const Home = ({ logout }) => {
           </div>
 
           <div className="card">
-            <p>Total Revenues</p>
-            <p>$2,129,430</p>
+            <p>Total Transactions</p>
+            <p>1,520</p>
             <i className="far fa-tags"></i>
           </div>
 
           <div className="card">
             <p>Total Revenues</p>
-            <p>$2,129,430</p>
+            <p>9,721</p>
             <i className="fas fa-thumbs-up"></i>
           </div>
 
           <div className="card">
-            <p>Total Revenues</p>
-            <p>$2,129,430</p>
+            <p>Total Users</p>
+            <p>892</p>
             <i className="far fa-user-friends"></i>
           </div>
         </div>
-
       </div>
     </div>
   );
