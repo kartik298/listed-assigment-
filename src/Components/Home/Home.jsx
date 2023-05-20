5c73181927d2fcfbca2823399b001f14cfc347e1
prashant kumar <prashantmanwan@gmail.com>
2023-05-20 18:54:56 +0530

    netlify fix 2

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { getAuth } from 'firebase/auth';
 import usersData from '../../Data/Users.json';
 import Sidebar from '../Sidebar/Sidebar';
@@ -11,6 +11,20 @@ const Home = ({ logout }) => {
   const [user, setUser] = useState(null);
   const [savedUsers, setSavedUsers] = useState(usersData);
   const auth = getAuth();
+
+  const saveUser = useCallback((user) => {
+    const newUser = {
+      id: user?.uid,
+      name: user?.displayName,
+      imageUrl: user?.photoURL,
+    };
+
+    const existingUser = savedUsers.find((existingUser) => existingUser.id === newUser.id);
+
+    if (!existingUser) {
+      setSavedUsers((prevUsers) => [...prevUsers, newUser]);
+    }
+  }, [savedUsers]);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -25,30 +39,15 @@ const Home = ({ logout }) => {
     });
 
     return () => unsubscribe();
-  }, [auth, saveUser]); 
+  }, [auth, saveUser]);
 
   const handleLogout = () => {
     logout();
   };
 
-  const userEmail = user?.email; // checking for null or undef... value
-  const userName = user?.displayName; // checking for null or undef... value
-  const userImage = user?.photoURL;  // checking for null or undef... value
-
-  const saveUser = (user) => {
-    const newUser = {
-      id: user?.uid,
-      name: user?.displayName,
-      imageUrl: user?.photoURL,
-    };
-
-    const existingUser = savedUsers.find((existingUser) => existingUser.id === newUser.id);
-
-    if (!existingUser) {
-      setSavedUsers((prevUsers) => [...prevUsers, newUser]);
-    }
-  };
-
+  const userEmail = user?.email;
+  const userName = user?.displayName;
+  const userImage = user?.photoURL;
 
   return (
     <div className='home'>
@@ -60,7 +59,6 @@ const Home = ({ logout }) => {
         <Charts />
         <p className='credit'>Developed by Prashant Kumar - https://enally.in/projects/</p>
       </div>
-      
     </div>
   );
 };
